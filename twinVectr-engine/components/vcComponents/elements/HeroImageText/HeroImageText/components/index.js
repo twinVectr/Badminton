6417f400679c03e5332ac6445251fd6c3e2e37fe
twinVectr <gokul2259@gmail.com>
2018-12-25 13:50:47 -0500

    fixed hero image grunt and css

--- twinVectr-engine/components/vcComponents/elements/HeroImageText/HeroImageText/components/index.js
+++ twinVectr-engine/components/vcComponents/elements/HeroImageText/HeroImageText/components/index.js
@@ -2,19 +2,22 @@ import React from 'react'
 import vcCake from 'vc-cake';
 const vcvAPI = vcCake.getService('api')
 
-export default class vcExampleComponent extends vcvAPI.elementComponent {
+export default class HeroImage extends vcvAPI.elementComponent {
 
   constructor(props) {
     super(props);
+    console.log(props);
   }
 
   render() {
     const { id, atts, editor } = this.props
-    const { height: value } = atts;
+    const { height: value, output, color } = atts;
     const doAll = this.applyDO('all');
 
     return <div {...editor} >
-      <div className="vceHeroImage--container" style={{ 'height': value }} id={'el-' + id}  {...doAll} />
+      <div className="vceHeroImage--container" style={{ 'height': value }} id={'el-' + id}  {...doAll} >
+        <p className="text-heading-lg" style={{ 'color': color }}>{output}</p>
+      </div>
     </div>
   }
 }
