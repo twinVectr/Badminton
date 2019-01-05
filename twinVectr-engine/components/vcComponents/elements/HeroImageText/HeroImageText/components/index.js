7307246789e079318a0dad6e02b41a3066c21413
twinVectr <gokul2259@gmail.com>
2019-01-04 23:35:17 -0500

    Hero Text initial set up

--- twinVectr-engine/components/vcComponents/elements/HeroImageText/HeroImageText/components/index.js
+++ twinVectr-engine/components/vcComponents/elements/HeroImageText/HeroImageText/components/index.js
@@ -1,23 +1,44 @@
 import React from 'react'
 import vcCake from 'vc-cake';
+import Button from '../../../../../../../frontend/components/Button';
 const vcvAPI = vcCake.getService('api');
 
 export default class HeroImage extends vcvAPI.elementComponent {
 
   constructor(props) {
     super(props);
-    this.onClick = this.onClick.bind(this);
+    this.heroText = React.createRef();
+    this.heroButton = React.createRef();
+  }
+
+  componentDidUpdate() {
+    const { buttonPosition, textPosition } = this.props.atts;
+    const btnPositions = buttonPosition.split(' ');
+    const txtPositions = textPosition.split(' ');
+
+    this.heroText.current.style.transform = `translate(${txtPositions[0]}, ${txtPositions[1]})`;
+    this.heroButton.current.style.transform = `translate(${btnPositions[0]}, ${btnPositions[1]})`;
   }
 
   render() {
     const { id, atts, editor } = this.props
-    const { height: value, output, color } = atts;
+    const { heroHeight, output, color, buttonColor, buttonText, buttonHref } = atts;
     const doAll = this.applyDO('all');
+    const textStyle = {
+      color,
+    };
 
     return (
       <div {...editor} >
-        <div className="vceHeroImage--container" style={{ 'height': value }} id={'el-' + id}  {...doAll} >
-          <div className="text-heading-lg" style={{ 'color': color }} > {output}</div>
+        <div className="vceHeroImage--container" style={{ 'height': `${heroHeight}px` }} id={'el-' + id}  {...doAll} >
+          <div className="text-heading-lg" ref={this.heroText} style={{ ...textStyle }} > {output}</div>
+          <div ref={this.heroButton}>
+            <Button
+              buttonText={buttonText}
+              buttonHref={buttonHref}
+              buttonColor={buttonColor}
+            />
+          </div>
         </div>
       </div>
     );
