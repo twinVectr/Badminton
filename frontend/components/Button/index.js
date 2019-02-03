1dc53b49d59e4ab8d7d1763b189c6183186ba6b1
twinVectr <gokul2259@gmail.com>
2019-02-02 19:51:51 -0500

    hero image - button link toggle

--- frontend/components/Button/index.js
+++ frontend/components/Button/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import classNames from 'classnames';
 const Button = (props) => {
-  const { buttonText, buttonHref, buttonColor } = props;
+  const { buttonText, buttonHref, buttonColor, type } = props;
+  const classes = classNames(
+    'tv-button',
+    {
+      [`tv-button-${type}`]: type === 'link',
+    }
+  );
+
+  const style = {
+    backgroundColor: (type !== 'link') ? buttonColor : null,
+  };
+
   return (
     <div>
-      <a className="tv-button" href={buttonHref} style={{ backgroundColor: buttonColor }}> {buttonText} </a>
+      <a className={classes} href={buttonHref} style={style}> {buttonText} </a>
     </div>
   );
 }
